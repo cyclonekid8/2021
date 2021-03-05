9db27b3449d115768d8ebf3ebe169a74dd0c15c8
cyclonekid8 <muzafarshahmarican@gmail.com>
2021-03-05 18:27:21 +0800

    fixed balance

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -6,17 +6,24 @@ import LoginAPI from '../api/LoginAPI';
 class Balance extends React.Component {
     
 
-    state={balance:''};
+    state={assetbalance:'',cashbalance:'',clicked:false};
     
-    Balances = async() => {
+    Balances = async(obj) => {
         
-        const obj=this.props.location.state.data;
+        if(this.state.clicked===true) {
+            this.setState({clicked:false}); 
+        }
+        else {
+            this.setState({clicked:true});
+        }
         console.log(obj);
         const details= await LoginAPI.post("/balance",{
             'accountKey':obj
         });
         console.log(details);
-        this.setState({balance:details});
+        this.setState({assetbalance:details.data.assetBalance});
+        this.setState({cashbalance:details.data.cashBalance})
+        console.log(details);
        
     }
     
@@ -24,16 +31,22 @@ class Balance extends React.Component {
     render() {
 
     
-
-    
+        const obj=this.props.location.state.data;
         return (
-            <div>
-                <div >
-                    Balance
-                
-                </div>  
+            <div className="ui container">
+                {this.state.clicked ? 
+                <div>
+                    <div className="ui container">
+                        {this.state.assetbalance}
+                        <hr/>
+                        {this.state.cashbalance}
+                    </div>
+                    <button onClick={(e)=>this.Balances(obj)}>Hide</button>
+                </div>:<button onClick={(e)=>this.Balances(obj)}>Show</button>}
             </div>
-        );
+
+        )
     }
 }
+
 export default withRouter(Balance);
